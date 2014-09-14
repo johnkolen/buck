e09d6a700c3c5ea8a84c475cc60afc37ccb1f483
John Kolen <johnfkolen@gmail.com>
2014-09-13 18:53:10 -0700

    user validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,25 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "create empty user" do
+    user = User.create
+    assert user.new_record?
+  end
+  [:first_name, :last_name, :email].each do |tgt|
+    test "create missing #{tgt}" do
+      h = new_user_params
+      h.delete tgt
+      user = User.create h
+      assert user.new_record?
+    end
+  end
+
+  test "create" do
+    user = User.create new_user_params
+    assert user.persisted?
+  end
+
+  def new_user_params
+    {:first_name=>"Alan", :last_name=>"Archer", :email=>"alan@mail.com"}
+  end
 end
