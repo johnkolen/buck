8ec566955291da9d89929e28821338a04bbd4611
John Kolen <johnfkolen@gmail.com>
2014-09-13 18:19:34 -0700

    default avatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,4 +6,8 @@ module UsersHelper
   def user_edit_fields form
     edit_field form, [:first_name, :last_name, :email]
   end
+
+  def show_avatar user, size=nil
+    image_tag "generic_avatar.png"
+  end
 end
