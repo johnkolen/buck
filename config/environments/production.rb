8a7bbe7f37339e1a0e4832b4c3e52548e61050cc
John Kolen <johnfkolen@gmail.com>
2014-09-20 15:45:16 -0700

    s3 on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,9 @@ Rails.application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket=>'buck-production'
+      :bucket=>'buck-production',
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
 end
