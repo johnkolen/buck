e0c3c51a35917ea22fdc0c54e0b7d781e54be84d
John Kolen <johnfkolen@gmail.com>
2015-01-25 16:24:57 -0800

    #54: email handles https and added image to mail in staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -60,6 +60,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: 'betuabuck.herokuapp.com' }
+  config.action_mailer.asset_host = "http://betuabuck.herokuapp.com"
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
